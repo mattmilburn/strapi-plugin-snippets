d77cde9434739be47f123e383b6fc60571f7f36c
Matt Milburn <me@mattmilburn.com>
2023-03-19 15:49:11 -0500

    Create extra service methods

--- server/services/snippets.js
+++ server/services/snippets.js
@@ -11,4 +11,30 @@ module.exports = ( { strapi } ) => ( {
         [ result.code ]: result.replacement,
       } ), {} ) );
   },
+
+  getStringAttrs( uid ) {
+    const schema = strapi.contentTypes[ uid ];
+
+    if ( ! schema ) {
+      return [];
+    }
+
+    const attrs = schema.attributes;
+
+    return Object.keys( attrs ).filter( key => {
+      return [ 'customField', 'richtext', 'string', 'text' ].includes( attrs[ key ].type );
+    } );
+  },
+
+  parseAttr( value ) {
+    /**
+     * @TODO - Parse `value` for `code` replacements.
+     */
+
+    return value;
+  },
+
+  uids() {
+    return Object.keys( strapi.contentTypes ).filter( key => key.includes( 'api::' ) );
+  },
 } );
