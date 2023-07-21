9f054286946045d76332d5c05eaf5097dcf0012b
Matt Milburn <me@mattmilburn.com>
2023-07-21 14:04:22 -0500

    Create initial tests for admin utils

---
+++ admin/src/utils/__tests__/sanitize-code.spec.js
@@ -0,0 +1,19 @@
+import sanitizeCode from '../sanitize-code';
+
+describe('sanitizeCode', () => {
+  it('should remove any non-alphanumeric characters', () => {
+    const input = 'example_123(&)?';
+    const output = 'example_123';
+    const result = sanitizeCode(input);
+
+    expect(result).toEqual(output);
+  });
+
+  it('should enforce uppercasing', () => {
+    const input = 'example123';
+    const output = 'EXAMPLE123';
+    const result = sanitizeCode(input, { uppercase: true });
+
+    expect(result).toEqual(output);
+  });
+});
