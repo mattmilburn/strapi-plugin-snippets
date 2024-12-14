7ec19811c28862230c8ab0e8914d466b5c2e0d68
Matt Milburn <me@mattmilburn.com>
2024-12-14 12:47:19 -0600

    Refactor transform middleware to work with new API format

--- server/src/middlewares/transformSnippets.ts
+++ server/src/middlewares/transformSnippets.ts
@@ -1,8 +1,5 @@
 import { type Core } from '@strapi/strapi';
-import has from 'lodash/has';
-import head from 'lodash/head';
 import isPlainObject from 'lodash/isPlainObject';
-import omit from 'lodash/omit';
 
 import { getService, interpolate, isApiRequest } from '../utils';
 
@@ -14,41 +11,12 @@ const transform = (data: object, config, snippets): any => {
     return data;
   }
 
-  // Entity or relation wrapper.
-  if (has(data, 'data')) {
-    return {
-      ...data,
-      data: transform(data.data, config, snippets),
-    };
-  }
-
-  // Entity or relation data.
-  if (has(data, 'attributes')) {
-    return {
-      ...data,
-      attributes: transform(data.attributes, config, snippets),
-    };
-  }
-
-  // Support strapi-plugin-transformer features which might remove `attributes`
-  // and `data` wrappers.
-  if (has(data, 'id')) {
-    return {
-      ...data,
-      // Must omit the id otherwise it creates an infinite loop.
-      ...transform(omit(data, 'id'), config, snippets),
-    };
-  }
-
-  // Collection of entities, relations, or components.
-  if (
-    Array.isArray(data) &&
-    (has(head(data), 'attributes') || has(head(data), 'id') || has(head(data), '__component'))
-  ) {
+  // Transform arrays of data.
+  if (Array.isArray(data)) {
     return data.map((item) => transform(item, config, snippets));
   }
 
-  // Loop to transform properties.
+  // Transform object properties.
   if (isPlainObject(data)) {
     Object.entries(data).forEach(([key, value]) => {
       if (ignoreFields.includes(key)) {
@@ -72,16 +40,16 @@ const transformMiddleware = async (strapi: Core.Strapi) => {
   strapi.server.use(async (ctx, next) => {
     await next();
 
-    if (!ctx.body || !ctx.body.data || !isApiRequest(ctx)) {
+    if (!ctx?.body?.data || !isApiRequest(ctx)) {
       return;
     }
 
-    // Do nothing if this model is not supported or there are no snippets in the database.
+    const snippets = await getService('snippets').get();
     const config = await getService('config').get();
     const uids = await getService('config').uids();
     const uid = uids.find((_uid) => ctx.state.route.handler.includes(_uid));
-    const snippets = await getService('snippets').get();
 
+    // Do nothing if this model is not supported or there are no snippets in the database.
     if (!uid || !snippets) {
       return;
     }
